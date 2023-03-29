acf089258c4af469ff8629fd8b5c9edc688e997d
hcags <120849679+hcags@users.noreply.github.com>
2023-03-29 14:59:26 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    hearing = !(hearing)
+    if (hearing) {
+        basic.setLedColor(0x00ff00)
+    } else {
+        basic.setLedColor(0xff0000)
+    }
+})
+let hearing = false
+basic.showLeds(`
+    . . . . .
+    # # . # #
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.setLedColor(0xff0000)
+hearing = false
 basic.forever(function () {
 	
 })
