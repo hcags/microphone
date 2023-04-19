20324aa91e94d5b389842b36831fcf8eff88f2c8
hcags <120849679+hcags@users.noreply.github.com>
2023-04-19 12:11:27 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,12 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    if (input.soundLevel() < 50) {
-        basic.showLeds(`
-            . . . . .
-            # # . # #
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-    } else if (input.soundLevel() >= 50 && input.soundLevel() < 125) {
-        basic.showLeds(`
-            # # . # #
-            # # . # #
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-    } else if (input.soundLevel() >= 125 && input.soundLevel() < 190) {
-        basic.showLeds(`
-            . # . # .
-            # . . . #
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
+    hearing = !(hearing)
+    if (hearing) {
+        basic.setLedColor(0x00ff00)
     } else {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
+        basic.setLedColor(0xff0000)
     }
 })
+let hearing = false
 basic.showLeds(`
     . . . . .
     # # . # #
@@ -40,7 +14,42 @@ basic.showLeds(`
     # . . . #
     . # # # .
     `)
+hearing = false
 basic.setLedColor(0xff0000)
 basic.forever(function () {
-	
+    if (hearing || input.buttonIsPressed(Button.AB)) {
+        if (input.soundLevel() < 50) {
+            basic.showLeds(`
+                . . . . .
+                # # . # #
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+        } else if (input.soundLevel() >= 50 && input.soundLevel() < 125) {
+            basic.showLeds(`
+                # # . # #
+                # # . # #
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+        } else if (input.soundLevel() >= 125 && input.soundLevel() < 190) {
+            basic.showLeds(`
+                . # . # .
+                # . . . #
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+        } else {
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+        }
+    }
 })
